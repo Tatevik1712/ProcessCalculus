234232c9fdc115a9fc20bdd70b45bcef28e235b7
Tatevik1712 <tatev20050716@gmail.com>
2026-05-19 12:49:52 +0900

    Комментарии

--- task_3/scraper/network.py
+++ task_3/scraper/network.py
@@ -13,8 +13,8 @@ def get_html(url: str) -> str:
         if response.status_code == 200:
             return response.text
         else:
-            print(f"[Ошибка] Статус ответа: {response.status_code} для URL: {url}")
+            print(f"Ошибка. Статус ответа: {response.status_code} для URL: {url}")
             return ""
     except requests.RequestException as e:
-        print(f"[Ошибка сети]: {e}")
+        print(f"Ошибка сети: {e}")
         return ""
